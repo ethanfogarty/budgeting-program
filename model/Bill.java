7cf0cd1d2de0e509a90c0f09ce85133b1606bbfe
ethanfogarty <ethanfogarty2101@gmail.com>
2023-04-12 11:37:27 -0500

    Updates to Design Description and RenderText moved to model

--- model/Bill.java
+++ model/Bill.java
@@ -2,9 +2,6 @@ package model;
 
 import java.awt.Graphics2D;
 import java.util.ArrayList;
-
-import view.RenderText;
-
 import java.awt.Color;
 
 public class Bill extends Noun{
@@ -12,9 +9,10 @@ public class Bill extends Noun{
     private Float cost;
     private String name;
     private String dueDate;
-    private static final int size = 14;
+    private static final int size = 10;
+    private static Color color = Color.green;
 
-    public Bill(Float cost, String name, String dueDate, int x, int y, Color color){
+    public Bill(Float cost, String name, String dueDate, int x, int y){
         super(x, y, color);
         this.cost = cost;
         this.name = name;
@@ -25,8 +23,8 @@ public class Bill extends Noun{
     public void render(Graphics2D g2) {
         ArrayList<Noun> words = new ArrayList<Noun>();
         RenderText w1 = new RenderText(name, size, x, y, color);
-        RenderText w2 = new RenderText(cost.toString(), size, x, y, color);
-        RenderText w3 = new RenderText(dueDate, size, x, y, color);
+        RenderText w2 = new RenderText(cost.toString(), size, x, y + 10, color);
+        RenderText w3 = new RenderText(dueDate, size, x, w2.y + 10, color);
         words.add(w1);
         words.add(w2);
         words.add(w3);
